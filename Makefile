cb3f6937b246b020ed3499ecfd6ca022e39b405b
Giuseppe Manzo <beppe@my-distrobox.laptop>
2023-11-29 09:38:11 +0100

    editorRefreshScreen

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 
 kilo: kilo.c term.c
-	gcc -Wall kilo.c term.c
+	gcc -Wall -Wpedantic kilo.c term.c
--- kilo.c
+++ kilo.c
@@ -1,29 +1,37 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define CTRL_KEY(k)	((k) & 0x1F)
 
 extern void enableRawMode();
 
+extern char editorReadKey();
+
+void editorProcessKeypress() {
+	char c = editorReadKey();
+
+	switch(c) {
+		case CTRL_KEY('q'):
+			printf("Kilo: bye bye!\r\n");
+			exit(0);
+	}
+}
+
+void editorRefreshScreen() {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
 int main() {
 
 	enableRawMode();
+	editorRefreshScreen();
 
-	char c = '\0';
+
 	while( 1 ) {
-
-		read(STDIN_FILENO, &c, 1);
-		if( iscntrl(c) ) {
-			printf("%d\r\n", c);
-		} else {
-			printf("%x ('%c')\r\n", c, c);
-		}
-		
-		if ( c == CTRL_KEY('q' )) {
-			printf("Bye bye!\r\n");
-			break;
-		}
+		editorProcessKeypress();
 	}
 
 	return 0;
